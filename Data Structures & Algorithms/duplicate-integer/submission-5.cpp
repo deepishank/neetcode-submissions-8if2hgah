a199c82ec1a31231994f2bc368061070267a8431
Ishank Deep <86016773+deepishank@users.noreply.github.com>
2026-04-18 20:43:26 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int, int> mpp;
+        for(int i=0;i<n;i++){
+            if(mpp[nums[i]]>0){
+                return true;
+            }
+            mpp[nums[i]]++;
+
+        }
+        return false;
+    }
+};
